2426857384d9d1ebf5720177801b1c17f85a373c
brunocallachi <brunocallachi@hotmail.com>
2021-05-28 14:35:26 -0300

    28/05/21

--- blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
@@ -6,11 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity // essa classe vai ser uma entidade do JPA
@@ -31,6 +33,12 @@ public class Postagem {
 	
 	@Temporal(TemporalType.TIMESTAMP) // indica que estamos trabalhando com o tempo
 	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+		
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 
 	public long getId() {
 		return id;
@@ -65,6 +73,10 @@ public class Postagem {
 	}
 	
 	
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 	
 	
 	
